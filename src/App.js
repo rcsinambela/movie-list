4a18fa29897c0fbf415ea6da5b28d42260e9cc8e
Rycs123 <riyanda.c.sinambela@gmail.com>
2024-02-19 10:15:58 +0700

    remove unneeded file, code, and change source code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,169 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const tempMovieData = [
+    {
+        imdbID: "tt15398776",
+        Title: "Oppenheimer",
+        Year: "2013",
+        Poster: "https://m.media-amazon.com/images/M/MV5BMDBmYTZjNjUtN2M1MS00MTQ2LTk2ODgtNzc2M2QyZGE5NTVjXkEyXkFqcGdeQXVyNzAwMjU2MTY@._V1_SX300.jpg",
+    },
+    {
+        imdbID: "tt1517268",
+        Title: "Barbie",
+        Year: "2023",
+        Poster: "https://m.media-amazon.com/images/M/MV5BNjU3N2QxNzYtMjk1NC00MTc4LTk1NTQtMmUxNTljM2I0NDA5XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg",
+    },
+    {
+        imdbID: "tt8589698",
+        Title: "Teenage Mutant Ninja Turtles: Mutant Mayhem",
+        Year: "2023",
+        Poster: "https://m.media-amazon.com/images/M/MV5BYzE4MTllZTktMTIyZS00Yzg1LTg1YzAtMWQwZTZkNjNkODNjXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_SX300.jpg",
+    },
+];
+
+const tempWatchedData = [
+    {
+        imdbID: "tt15398776",
+        Title: "Oppenheimer",
+        Year: "2013",
+        Poster: "https://m.media-amazon.com/images/M/MV5BMDBmYTZjNjUtN2M1MS00MTQ2LTk2ODgtNzc2M2QyZGE5NTVjXkEyXkFqcGdeQXVyNzAwMjU2MTY@._V1_SX300.jpg",
+        runtime: 180,
+        imdbRating: 8.6,
+        userRating: 10,
+    },
+    {
+        imdbID: "tt1517268",
+        Title: "Barbie",
+        Year: "2023",
+        Poster: "https://m.media-amazon.com/images/M/MV5BNjU3N2QxNzYtMjk1NC00MTc4LTk1NTQtMmUxNTljM2I0NDA5XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg",
+        runtime: 114,
+        imdbRating: 7.2,
+        userRating: 8,
+    },
+];
+
+const average = (arr) =>
+    arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+
+export default function App() {
+    const [query, setQuery] = useState("");
+    const [movies, setMovies] = useState(tempMovieData);
+    const [watched, setWatched] = useState(tempWatchedData);
+    const [isOpen1, setIsOpen1] = useState(true);
+    const [isOpen2, setIsOpen2] = useState(true);
+
+    const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
+    const avgUserRating = average(watched.map((movie) => movie.userRating));
+    const avgRuntime = average(watched.map((movie) => movie.runtime));
+
+    return (
+        <>
+            <nav className="nav-bar">
+                <div className="logo">
+                    <span role="img">🎫</span>
+                    <h1>Movie</h1>
+                </div>
+                <input
+                    className="search"
+                    type="text"
+                    placeholder="Search movies..."
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                />
+                <p className="num-results">
+                    Found <strong>{movies.length}</strong> results
+                </p>
+            </nav>
+
+            <main className="main">
+                <div className="box">
+                    <button
+                        className="btn-toggle"
+                        onClick={() => setIsOpen1((open) => !open)}
+                    >
+                        {isOpen1 ? "–" : "+"}
+                    </button>
+                    {isOpen1 && (
+                        <ul className="list">
+                            {movies?.map((movie) => (
+                                <li key={movie.imdbID}>
+                                    <img
+                                        src={movie.Poster}
+                                        alt={`${movie.Title} poster`}
+                                    />
+                                    <h3>{movie.Title}</h3>
+                                    <div>
+                                        <p>
+                                            <span>📅</span>
+                                            <span>{movie.Year}</span>
+                                        </p>
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
+                    )}
+                </div>
+
+                <div className="box">
+                    <button
+                        className="btn-toggle"
+                        onClick={() => setIsOpen2((open) => !open)}
+                    >
+                        {isOpen2 ? "–" : "+"}
+                    </button>
+                    {isOpen2 && (
+                        <>
+                            <div className="summary">
+                                <h2>Movies you watched</h2>
+                                <div>
+                                    <p>
+                                        <span>#️⃣</span>
+                                        <span>{watched.length} movies</span>
+                                    </p>
+                                    <p>
+                                        <span>🎬</span>
+                                        <span>{avgImdbRating}</span>
+                                    </p>
+                                    <p>
+                                        <span>🌟</span>
+                                        <span>{avgUserRating}</span>
+                                    </p>
+                                    <p>
+                                        <span>⏳</span>
+                                        <span>{avgRuntime} min</span>
+                                    </p>
+                                </div>
+                            </div>
+
+                            <ul className="list">
+                                {watched.map((movie) => (
+                                    <li key={movie.imdbID}>
+                                        <img
+                                            src={movie.Poster}
+                                            alt={`${movie.Title} poster`}
+                                        />
+                                        <h3>{movie.Title}</h3>
+                                        <div>
+                                            <p>
+                                                <span>🎬</span>
+                                                <span>{movie.imdbRating}</span>
+                                            </p>
+                                            <p>
+                                                <span>🌟</span>
+                                                <span>{movie.userRating}</span>
+                                            </p>
+                                            <p>
+                                                <span>⏳</span>
+                                                <span>{movie.runtime} min</span>
+                                            </p>
+                                        </div>
+                                    </li>
+                                ))}
+                            </ul>
+                        </>
+                    )}
+                </div>
+            </main>
+        </>
+    );
 }
-
-export default App;
