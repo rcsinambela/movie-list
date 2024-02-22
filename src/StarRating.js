81182b8a071da83714bc1f8ec511939944e812b8
Rycs123 <riyanda.c.sinambela@gmail.com>
2024-02-22 19:26:43 +0700

    Added StarRating component and loading bar to the project

--- src/StarRating.js
+++ src/StarRating.js
@@ -23,7 +23,7 @@ const starStyle = {
     cursor: "pointer",
 };
 
-function Star({ onRate, full, onHoverIn, onHoverOut }) {
+function Star({ onRate, full, onHoverIn, onHoverOut, color, size }) {
     return (
         <span
             style={starStyle}
@@ -35,9 +35,9 @@ function Star({ onRate, full, onHoverIn, onHoverOut }) {
             {full ? (
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="#ffa534"
-                    stroke="#ffa534"
+                    viewBox={`0 0 ${size} ${size}`}
+                    fill={color}
+                    stroke={color}
                 >
                     <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
                 </svg>
@@ -45,7 +45,7 @@ function Star({ onRate, full, onHoverIn, onHoverOut }) {
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
-                    viewBox="0 0 24 24"
+                    viewBox={`0 0 ${size + 4} ${size + 4}`}
                     stroke="#000"
                 >
                     <path
@@ -64,7 +64,7 @@ StarRating.propTypes = {
     max: PropTypes.number,
 };
 
-export default function StarRating({ max = 5 }) {
+export default function StarRating({ max = 5, color, size }) {
     const [rating, setRating] = useState(0);
     const [tempRating, setTempRating] = useState(0);
     function handleRating(rating) {
@@ -85,6 +85,8 @@ export default function StarRating({ max = 5 }) {
                             }
                             onHoverIn={() => setTempRating(i + 1)}
                             onHoverOut={() => setTempRating(0)}
+                            color={color}
+                            size={size}
                         />
                     </span>
                 ))}
