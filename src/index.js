56c806bc3222ab5a8e7c73d33355a3e9c49b92e4
Rycs123 <riyanda.c.sinambela@gmail.com>
2024-02-23 00:53:08 +0700

    Add StarRating component

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
+    <>
         <App />
-    </React.StrictMode>
+        {/* <StarRating max={5} color="#ffa534" size={24} />
+        <StarRating max={10} color="#ffa534" size={20} /> */}
+    </>
 );
